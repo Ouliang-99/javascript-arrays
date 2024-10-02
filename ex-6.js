15e4ec901cbe334eef02fc446fc0ed0b431c9892
podusit <d.rimdusit@gmail.com>
2024-10-02 14:13:53 +0700

    hackhour:js array ex1-7

--- ex-6.js
+++ ex-6.js
@@ -4,7 +4,8 @@ const student = {
   subjects: ["Math", "Science", "History"],
 };
 
-console.log("Student Name: " + stdent.name);
-console.log("Student Age: " + student.age);
-console.log("Student Grade: " + student.gade);
+student.Age = 18; 
+console.log("Student Name: " + student.name);
+console.log("Student Age: " + student.Age);
+console.log("Student Grade: " + student.grade);
 console.log("Student Subjects: " + student.subjects);
